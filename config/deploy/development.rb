622e2eba379fcf8a823adacdbb2dae8ed3456186
MR.Y <programming@yatsukouji.com>
2015-09-19 23:48:07 +0900

    ぐちゃぐちゃ

---
+++ config/deploy/development.rb
@@ -0,0 +1,12 @@
+set :stage, :development
+role :app, %w{root@160.16.109.151}
+role :web, %w{root@$160.16.109.151}
+role :db, %w{root@$160.16.109.151}
+
+server '160.16.109.151',
+user: 'root',
+roles: %w{web app db},
+ssh_options: {
+ auth_methods: %w(password),
+ password: ''
+}
---
+++ vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.6.2/ext/nokogiri/siteconf20150919-62508-zmt4yf.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150919-62508-1r3sltk"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
